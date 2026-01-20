e72501296a328f0b32ae2ccf5e0f0080ff0137e7
Yury Gribov <tetra2005@gmail.com>
2026-01-21 00:50:42 +0300

    Test with different compilers.

--- tests/1/run.sh
+++ tests/1/run.sh
@@ -5,7 +5,9 @@ if set -o | grep -q pipefail; then set -o pipefail; fi
 
 cd $(dirname $0)
 
-gcc -c lib.c
+CC=${CC:-gcc}
+
+$CC -c lib.c
 ar rcs lib.a lib.o
-gcc -c main.c
-gcc main.o lib.a
+$CC -c main.c
+$CC main.o lib.a
